b17bcc96ef4e8b5df4aeeab8bfa455b23e1aa5e8
Mohammad Yusuf Khan <98419143+m-y-k@users.noreply.github.com>
2023-06-25 05:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+let a = new Promise(resolve => {
+	resolve("a");
+}, 1000);
+promises.push(a);
+let b = new Promise(resolve => {
+	resolve("b");
+}, 2000);
+promises.push(b);
+let c = new Promise(resolve => {
+	resolve("c");
+}, 3000);
+promises.push(c);
+let d = new Promise(resolve => {
+	resolve("d");
+}, 4000);
+promises.push(d);
+let e = new Promise(resolve => {
+	resolve("e");
+}, 5000);
+promises.push(e);
+
+let x = Promise.any(window.promises);
+
+const div = document.getElementById("output");
+div.innerText = x;
+document.body.appendChild(div);
