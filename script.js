e024220f5d51c63b9b3c3f9f6c11f92229d15f4d
Mohammad Yusuf Khan <98419143+m-y-k@users.noreply.github.com>
2023-06-25 06:00:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,28 +4,33 @@ window.promises = [];
 // add your promises to the array `promises`
 
 let a = new Promise(resolve => {
-	resolve("a");
-}, 1000);
-promises.push(a);
+	setTimeout(() => {
+		resolve("a");
+	}, 1000);
+});
 let b = new Promise(resolve => {
-	resolve("b");
-}, 2000);
-promises.push(b);
+	setTimeout(() => {
+		resolve("a");
+	}, 2000);
+});
 let c = new Promise(resolve => {
-	resolve("c");
-}, 3000);
-promises.push(c);
+	setTimeout(() => {
+		resolve("a");
+	}, 3000);
+});
 let d = new Promise(resolve => {
-	resolve("d");
-}, 4000);
-promises.push(d);
+	setTimeout(() => {
+		resolve("a");
+	}, 4000);
+});
 let e = new Promise(resolve => {
-	resolve("e");
-}, 5000);
-promises.push(e);
-
-let x = Promise.any(window.promises);
-
-const div = document.getElementById("output");
-div.innerText = x;
-document.body.appendChild(div);
+	setTimeout(() => {
+		resolve("a");
+	}, 5000);
+});
+let x = Promise.any([a, b, c, d, e]);
+window.promises = [a, b, c, d, e];
+x.then((data) => {
+    const ele = document.getElementById("output");
+	ele.innerText = data;
+  });
